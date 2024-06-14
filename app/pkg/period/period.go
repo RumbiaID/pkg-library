a63cfea7fae8120bc33b155eec95189b04e45839
ZinedineR <zinedin505@gmail.com>
2024-06-14 18:12:03 +0700

    add period and ratio

---
+++ app/pkg/period/period.go
@@ -0,0 +1,6 @@
+package period
+
+var PeriodSymbol = map[string]int{
+	"Monthly": 12,
+	"Yearly":  1,
+}
---
+++ app/pkg/ratio/ratio.go
@@ -0,0 +1,10 @@
+package ratiopkg
+
+var Operator = map[string]string{
+	"Equal":                 "=",
+	"Less than":             "<",
+	"Less than equal to":    "<=",
+	"Greater than":          ">",
+	"Greater than equal to": ">=",
+	"Between":               "range",
+}
