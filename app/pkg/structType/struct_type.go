9be3579efa59ec821097b47dc1b3e3db19c598af
hasifpri <hasifpriyambudi@outlook.com>
2024-07-03 10:12:35 +0700

    [ADD] [suffix for int64 to bigint]

--- app/pkg/structType/struct_type.go
+++ app/pkg/structType/struct_type.go
@@ -38,7 +38,7 @@ func GetType(dbType string, x interface{}, dst []string) ([]string, []string) {
 						selected2 = append(selected2, field+"::int")
 					case reflect.Int64:
 						selected = append(selected, "(new_value->>'"+field+"')::bigint AS "+field)
-						selected2 = append(selected2, field+"::int")
+						selected2 = append(selected2, field+"::bigint")
 					default:
 						selected = append(selected, "(new_value->>'"+field+"')::"+fieldType.Name()+" AS "+field)
 						selected2 = append(selected2, field+"::"+fieldType.Name())
