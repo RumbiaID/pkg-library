507401c48501076005fef709c9254c6464d9d4ec
ZinedineR <zinedin505@gmail.com>
2024-07-26 17:05:32 +0700

    [update as twilio client]

--- app/pkg/twiliotext/twiliotext.go
+++ app/pkg/twiliotext/twiliotext.go
@@ -3,18 +3,39 @@ package twiliotext
 import (
 	"github.com/twilio/twilio-go"
 	twilioApi "github.com/twilio/twilio-go/rest/api/v2010"
-	"os"
 )
 
-func SendMessage(body, to string) (*twilioApi.ApiV2010Message, error) {
+type Config struct {
+	TwilioAccountSid string
+	TwilioAuthToken  string
+	TwilioNumber     string
+}
+
+type TwilioText struct {
+	config *Config
+}
+
+type TwilioMethod interface {
+	SendMessage(body, to string) (*twilioApi.ApiV2010Message, error)
+	CheckMessage(sid string) (*twilioApi.ApiV2010Message, error)
+}
+
+func NewTwilioText(config *Config) TwilioMethod {
+	return &TwilioText{config: config}
+}
+
+// Username: os.Getenv("TWILIO_ACC_SID"),
+// Password: os.Getenv("TWILIO_AUTH_TOKEN"),
+// params.SetFrom(os.Getenv("TWILIO_NUMBER"))
+func (t *TwilioText) SendMessage(body, to string) (*twilioApi.ApiV2010Message, error) {
 	client := twilio.NewRestClientWithParams(twilio.ClientParams{
-		Username: os.Getenv("TWILIO_ACC_SID"),
-		Password: os.Getenv("TWILIO_AUTH_TOKEN"),
+		Username: t.config.TwilioAccountSid,
+		Password: t.config.TwilioAuthToken,
 	})
 
 	params := &twilioApi.CreateMessageParams{}
 	params.SetBody(body)
-	params.SetFrom(os.Getenv("TWILIO_NUMBER"))
+	params.SetFrom(t.config.TwilioNumber)
 	params.SetTo(to)
 
 	resp, err := client.Api.CreateMessage(params)
@@ -24,14 +45,14 @@ func SendMessage(body, to string) (*twilioApi.ApiV2010Message, error) {
 	return resp, nil
 }
 
-func CheckMessage(sid string) (*twilioApi.ApiV2010Message, error) {
+func (t *TwilioText) CheckMessage(sid string) (*twilioApi.ApiV2010Message, error) {
 	client := twilio.NewRestClientWithParams(twilio.ClientParams{
-		Username: os.Getenv("TWILIO_ACC_SID"),
-		Password: os.Getenv("TWILIO_AUTH_TOKEN"),
+		Username: t.config.TwilioAccountSid,
+		Password: t.config.TwilioAuthToken,
 	})
 
 	params := &twilioApi.FetchMessageParams{}
-	params.SetPathAccountSid(os.Getenv("TWILIO_ACC_SID"))
+	params.SetPathAccountSid(t.config.TwilioNumber)
 
 	resp, err := client.Api.FetchMessage(sid, params)
 	if err != nil {
